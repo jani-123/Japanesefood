bba370ef19ce77aa6fe4807bb63f65eb02b24a75
KelyKley <kelyanamurohuamani@gmail.com>
2017-10-26 16:31:06 -0500

    agregando

--- src/menu.js
+++ src/menu.js
@@ -5,13 +5,15 @@ export const Menu = ({food}) => {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-sm-4">{food[0].img}</div>
-                <div className="col-sm-4">
+                <div className="col-md-1 col-xs-1 col-sm-1 k-back"></div>
+                <div className="col-sm-3 col-md-3 col-xs-12"><img className="k-imgDetails img-responsive" src={food[0].img}/></div>
+                <div className="col-sm-4 col-md-4 col-xs-12">
                     <Description foodDetail={food[0]}/>
                 </div>
-                <div className="col-sm-4">
+                <div className="col-sm-3 col-md-3 col-xs-12">
                     <Nutrition foodDetail={food[0]}/>
                 </div>
+                <div className="col-md-1 col-xs-1 col-sm-1 k-next"></div>
             </div>
         </div>
     )
@@ -28,12 +30,41 @@ const Description =({foodDetail}) => {
     )
 }
 const Nutrition = ({foodDetail}) => {
-    let list = foodDetail.nutritional.map((elemen, index) => {
-        return (
-            <li>
-                {elemen}
-            </li>
-        )
-    });
-    return (<ul>{list}</ul>)
+    return (
+        <div className="row">
+            <table class="table table-responsive">
+                <thead>
+                    <tr>
+                    <th colspan="2">Nutritional Information</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>Protein</td>
+                    <td className="text-right">{foodDetail.nutritional[0]}</td>
+                    </tr>
+                    <tr>
+                    <td>Fat (total)</td>
+                    <td className="text-right">{foodDetail.nutritional[1]}</td>
+                    </tr>
+                    <tr>
+                    <td>Carbohydrate</td>
+                    <td className="text-right">{foodDetail.nutritional[2]}</td>
+                    </tr>
+                    <tr>
+                    <td>Energy (kJ)</td>
+                    <td className="text-right">{foodDetail.nutritional[2]}</td>
+                    </tr>
+                    <tr>
+                    <td>Energy (kcal)</td>
+                    <td className="text-right">{foodDetail.nutritional[2]}</td>
+                    </tr>
+                    <tr>
+                    <td>Sugar</td>
+                    <td className="text-right">{foodDetail.nutritional[2]}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 }
