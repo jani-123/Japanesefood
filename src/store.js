0f7534039ec7c60a16f041119a2e83c9fa90bd9c
Mariley20 <mariley2018@gmail.com>
2017-10-26 14:41:21 -0500

    merge master

--- src/store.js
+++ src/store.js
@@ -1,19 +1,6 @@
 import createStore from "redux-zero";
 
 let FOOD = [{
-        name: "sushi",
-        price: 2.00,
-        description: "plato tradicional japones",
-        nutritional: [{
-            protein: 2.100,
-            fat: 0.4433,
-            carbohydrate: 0.600,
-            energyKl: 7.3000,
-            energyKcal: 3.344,
-            sugar: 0.344
-        }]
-    },
-    {
         name: "Sashimi salad",
         price: 12,
         description: "Organic greens topped with fresh sashimi, wasabi soy vinaigrette.",
