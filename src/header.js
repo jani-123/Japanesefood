d07185ffa4193ae752fad8e5927892cad9999dc8
Mariley20 <mariley2018@gmail.com>
2017-10-26 17:14:45 -0500

    header component

---
+++ src/header.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { nextFood } from './actionsMariley';
+export const Header = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
