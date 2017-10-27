809cddf6c0b24185e62e5d3f4b1b9024ae55aa90
jani-123 <janina.rqa@gmail.com>
2017-10-27 12:18:55 -0500

    creando gh-pages

--- src/header.js
+++ src/header.js
@@ -4,7 +4,7 @@ import './header.css';
 export const Header = () => {
     return (
             <div id="mar-header">
-                    <h1 class="logo"><a href="#/"></a></h1>
+                    <h1 className="logo"><a href="#/"></a></h1>
                     <a id="cart-info" href="#">Shopping Cart
                         <span><span data-bind="text: cart.contentsCount">0</span> items</span>
                     </a>
