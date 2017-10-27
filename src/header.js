7e7a33970becb53c0ea6f99132d936776c2b3251
Mariley20 <mariley2018@gmail.com>
2017-10-27 14:05:31 -0500

    header

--- src/header.js
+++ src/header.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './header.css';
+import { NavLink} from 'react-router-dom';
 export const Header = ({cart}) => {
     return (
             <div id="mar-header">
                     <h1 className="logo"><a href="#/"></a></h1>
                     <div className="btn-header">
-                        <a id="cart-info" href="#">Shopping Cart
+                        <NavLink to="/viewproduct" id="cart-info"> 
+                        {/* <a id="cart-info" href="#">Shopping Cart */}
                             <span><span data-bind="text: cart.contentsCount">{cart.length}</span> items</span>
-                        </a>
+                        {/* </a> */}
+                        </NavLink>
                     </div>
             </div>
     )
