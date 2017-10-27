3654f768c32605eab28ad4526d2e6b4738248a97
Mariley20 <mariley2018@gmail.com>
2017-10-27 14:31:15 -0500

    estilos

--- src/header.js
+++ src/header.js
@@ -6,7 +6,7 @@ export const Header = ({cart}) => {
             <div id="mar-header">
                     <h1 className="logo"><a href="#/"></a></h1>
                     <div className="btn-header">
-                        <NavLink to="/viewproduct" id="cart-info"> 
+                        <NavLink to="/" id="cart-info"> Shopping Cart
                         {/* <a id="cart-info" href="#">Shopping Cart */}
                             <span><span data-bind="text: cart.contentsCount">{cart.length}</span> items</span>
                         {/* </a> */}
